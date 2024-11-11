66512074d36361d0dde12e2125dc27d90f235a61
huzefa26 <huzefa.jambughoda@gmail.com>
2024-11-11 22:31:21 +0400

    add netlify to support SSR deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,11 @@ import tailwind from '@astrojs/tailwind';
 
 import mdx from '@astrojs/mdx';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
   output: 'server',
+  adapter: netlify(),
 });
